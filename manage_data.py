1b27e093d0dc25b87cf9ef42c4cca4028d98dc83
wolfejar <wolfejar@ksu.edu>
2019-09-16 15:10:05 -0500

    Files for models and data management

---
+++ manage_data.py
@@ -0,0 +1,12 @@
+def add_images():
+    return
+
+def remove_images():
+    return
+
+def remove_friends():
+    return
+
+def add_friends():
+    return
+
---
+++ model.py
@@ -0,0 +1,6 @@
+def get_most_recent_model():
+    return
+
+def train_model():
+    return
+
