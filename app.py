29874ca8b4d2544a9931c35a48d9a06cf3d02412
wolfejar <wolfejar@ksu.edu>
2019-09-16 14:57:36 -0500

    Create dashboard and templates for main pages

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
+def index_get():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/train')
+def train():
+    return render_template('models.html')
+
+@app.route('/select_photos')
+def select_photos():
+    return render_template('friends.html')
+
+@app.route('/metrics')
+def metrics():
+    return render_template('metrics.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
