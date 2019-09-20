30b4a549fb5ce159d70510ebc5facbd48c37fd88
wolfejar <wolfejar@ksu.edu>
2019-09-20 15:49:14 -0500

    More additions to model training

--- models/model.py
+++ models/model.py
@@ -5,6 +5,8 @@ import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.autograd import Variable
 import torch.nn.functional as F
+import torch.optim as optim
+import time
 
 seed = 42
 np.random.seed(seed)
@@ -78,6 +80,102 @@ class SimpleCNN(torch.nn.Module):
 def output_size(in_size, kernel_size, stride, padding):
     output = int((in_size - kernel_size + 2*padding) / stride) + 1
     return output
+
+
+# DataLoader takes in a dataset and a sampler for loading (num_workers deals with system level memory)
+def get_train_loader(batch_size):
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, sampler=train_sampler, num_workers=2)
+    return train_loader
+
+
+# Test and validation loaders have constant batch sizes, so we can define them directly
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, sampler=test_sampler, num_workers=2)
+val_loader = torch.utils.data.DataLoader(train_set, batch_size=128, sampler=val_sampler, num_workers=2)
+
+
+def create_loss_and_optimizer(net, learning_rate=0.001):
+    # Loss function
+    loss = torch.nn.CrossEntropyLoss()
+
+    # Optimizer
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+
+    return loss, optimizer
+
+
+def train_net(net, batch_size, n_epochs, learning_rate):
+    # Print all of the hyperparameters of the training iteration:
+    print("===== HYPERPARAMETERS =====")
+    print("batch_size=", batch_size)
+    print("epochs=", n_epochs)
+    print("learning_rate=", learning_rate)
+    print("=" * 30)
+
+    # Get training data
+    train_loader = get_train_loader(batch_size)
+    n_batches = len(train_loader)
+
+    # Create our loss and optimizer functions
+    loss, optimizer = create_loss_and_optimizer(net, learning_rate)
+
+    # Time for printing
+    training_start_time = time.time()
+
+    # Loop for n_epochs
+    for epoch in range(n_epochs):
+
+        running_loss = 0.0
+        print_every = n_batches // 10
+        start_time = time.time()
+        total_train_loss = 0
+
+        for i, data in enumerate(train_loader, 0):
+
+            # Get inputs
+            inputs, labels = data
+
+            # Wrap them in a Variable object
+            inputs, labels = Variable(inputs), Variable(labels)
+
+            # Set the parameter gradients to zero
+            optimizer.zero_grad()
+
+            # Forward pass, backward pass, optimize
+            outputs = net(inputs)
+            loss_size = loss(outputs, labels)
+            loss_size.backward()
+            optimizer.step()
+
+            # Print statistics
+            running_loss += loss_size.data[0]
+            total_train_loss += loss_size.data[0]
+
+            # Print every 10th batch of an epoch
+            if (i + 1) % (print_every + 1) == 0:
+                print("Epoch {}, {:d}% \t train_loss: {:.2f} took: {:.2f}s".format(
+                    epoch + 1, int(100 * (i + 1) / n_batches), running_loss / print_every, time.time() - start_time))
+                # Reset running loss and time
+                running_loss = 0.0
+                start_time = time.time()
+
+        # At the end of the epoch, do a pass on the validation set
+        total_val_loss = 0
+        for inputs, labels in val_loader:
+            # Wrap tensors in Variables
+            inputs, labels = Variable(inputs), Variable(labels)
+
+            # Forward pass
+            val_outputs = net(inputs)
+            val_loss_size = loss(val_outputs, labels)
+            total_val_loss += val_loss_size.data[0]
+
+        print("Validation loss = {:.2f}".format(total_val_loss / len(val_loader)))
+
+    print("Training finished, took {:.2f}s".format(time.time() - training_start_time))
+
+
+CNN = SimpleCNN()
+train_net(CNN, batch_size=32, n_epochs=5, learning_rate=0.001)
 
 
 def get_most_recent_model():
